9afcd0287e98812d6185660b4b2250643ac7c7a1
valentinovaldebenito <valentinovaldebenito.dev@gmail.com>
2024-09-30 23:42:59 -0300

    Fix: Views

--- prime-react/src/views/Home.jsx
+++ prime-react/src/views/Home.jsx
@@ -1,36 +1,40 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Toolbar } from "primereact/toolbar";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { Avatar } from "primereact/avatar";
-import { SplitButton } from "primereact/splitbutton";
-import { InputText } from "primereact/inputtext";
-import { IconField } from "primereact/iconfield";
-import { InputIcon } from "primereact/inputicon";
 import "../index.css";
 
 function Home() {
-  const items = [
-    {
-      label: "Update",
-      icon: "pi pi-refresh",
-    },
-    {
-      label: "Delete",
-      icon: "pi pi-times",
-    },
-  ];
-  const startContent = (
-    <React.Fragment>
-      <Avatar image="../assets/linkedin_img.jpeg" />
-    </React.Fragment>
-  );
+  const navigate = useNavigate();
+
+  function navegar(ruta) {
+    navigate("/" + ruta);
+  }
+
+  const startContent = <Avatar image="../assets/linkedin_img.png" size="xlarge" shape="circle" className="bg-white" />;
 
   const endContent = (
     <React.Fragment>
-      <Button icon="pi pi-home" className="mr-2" />
-      <Button icon="pi pi-search" className="mr-2" />
-      <Button icon="pi pi-code" />
+      <Button
+        label="Inicio"
+        icon="pi pi-home"
+        className="mr-2 bg-green-600 text-white border-none"
+        onClick={() => navegar("")}
+      />
+      <Button
+        label="Sobre mí"
+        icon="pi pi-search"
+        className="mr-2 bg-green-600 text-white border-none"
+        onClick={() => navegar("about")}
+      />
+      <Button
+        label="Habilidades"
+        icon="pi pi-code"
+        className="bg-green-600 text-white border-none"
+        onClick={() => navegar("skills")}
+      />
     </React.Fragment>
   );
 
@@ -39,16 +43,24 @@ function Home() {
       <div className="w-full h-screen flex flex-column align-items-center bg-gray-100 p-0 m-0">
         <div className="w-full h-[10%] flex align-items-center m-0 p-0">
           <Toolbar
-            className="bg-blue-500 w-full h-auto flex align-items-center m-0"
+            className="bg-green-500 w-full h-auto flex align-items-center border-none border-noround m-0"
             start={startContent}
-            //center={centerContent}
             end={endContent}
           ></Toolbar>
         </div>
-        <div className="w-full h-full flex justify-content-center align-items-center">
-          <Card className="w-2 h-2 flex justify-content-center align-items-center">
-            <h1>Hola mundo</h1>
-          </Card>
+        <div className="w-full h-full flex flex-column justify-content-center align-items-center gap-4">
+          <h1>Inicio</h1>
+          <div className="w-full flex flex-row justify-content-center gap-4">
+            <Card className="w-3 h-26rem flex justify-content-center align-items-center">
+              <h1>Hola mundo</h1>
+            </Card>
+            <Card className="w-3 h-26rem flex justify-content-center align-items-center">
+              <h1>Hola mundo</h1>
+            </Card>
+            <Card className="w-3 h-26rem flex justify-content-center align-items-center">
+              <h1>Hola mundo</h1>
+            </Card>
+          </div>
         </div>
       </div>
     </>
