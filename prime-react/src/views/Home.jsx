5589ae9d703ee8fb0a7dbd00a3ffbb2d5754572c
valentinovaldebenito <valentinovaldebenito.dev@gmail.com>
2024-10-01 21:13:19 -0300

    Feat: Imagenes Card Inicio

--- prime-react/src/views/Home.jsx
+++ prime-react/src/views/Home.jsx
@@ -5,6 +5,8 @@ import Navbar from "../components/Navbar";
 import "../index.css";
 
 function Home() {
+  const imgCard = new URL('../assets/programming-background-concept.jpg', import.meta.url).href;
+
   return (
     <>
       <div className="w-full h-screen flex flex-column align-items-center bg-gray-100 p-0 m-0">
@@ -12,9 +14,9 @@ function Home() {
         <div className="w-full h-full flex flex-column justify-content-center align-items-center gap-4">
           <h1>Inicio</h1>
           <div className="w-full flex flex-row justify-content-center gap-4">
-            <Card className="w-3 h-26rem flex flex-column align-items-center">
-              <div className="flex w-full justify-content-center">
-                <Image src="" width="200px" height="200px" />
+            <Card className="w-3 h-26rem flex flex-column align-items-center p-2 hover:shadow-4 cursor-pointer">
+              <div className="flex w-full justify-content-center px-4 mb-4">
+                <Image src={imgCard} width="100%"/>
               </div>
               <div className="w-full">
                 <span>
@@ -23,11 +25,27 @@ function Home() {
                 </span>
               </div>
             </Card>
-            <Card className="w-3 h-26rem flex justify-content-center align-items-center">
-              <h1>Hola mundo</h1>
+            <Card className="w-3 h-26rem flex flex-column align-items-center p-2 hover:shadow-4 cursor-pointer">
+              <div className="flex w-full justify-content-center px-4 mb-4">
+                <Image src={imgCard} width="100%"/>
+              </div>
+              <div className="w-full">
+                <span>
+                  Me llamo Valentino Valdebenito, soy un desarrollador Full-Stack, actualmente cursando la carrera de
+                  Técnico Superior en Desarrollo de Software Full-Stack en el Instituto Superior Cipolletti.
+                </span>
+              </div>
             </Card>
-            <Card className="w-3 h-26rem flex justify-content-center align-items-center">
-              <h1>Hola mundo</h1>
+            <Card className="w-3 h-26rem flex flex-column align-items-center p-2 hover:shadow-4 cursor-pointer">
+              <div className="flex w-full justify-content-center px-4 mb-4">
+                <Image src={imgCard} width="100%"/>
+              </div>
+              <div className="w-full">
+                <span>
+                  Me llamo Valentino Valdebenito, soy un desarrollador Full-Stack, actualmente cursando la carrera de
+                  Técnico Superior en Desarrollo de Software Full-Stack en el Instituto Superior Cipolletti.
+                </span>
+              </div>
             </Card>
           </div>
         </div>
